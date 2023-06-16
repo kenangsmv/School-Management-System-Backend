95ca615269ad2bef7937dd873e12defcbb784be0
kanangsmv <rio.kenan@gmail.com>
2023-06-16 16:49:23 +0300

    token generation and verification

---
+++ middlewares/isLogin.js
@@ -0,0 +1,23 @@
+const Admin = require("../model/Staff/Admin")
+const verifyToken = require("../utils/verifyToken");
+
+const isLogin = async (req, res, next) => {
+  // get token from header
+    const headerObj = req.headers;
+    const token = headerObj.authorization.split(" ")[1];
+    const verifiedToken = verifyToken(token);
+
+  // verify token
+    if (verifiedToken) {
+        //find the admin
+        const user = await Admin.findById(verifiedToken.id).select("name email role");
+  // save the user into request object;
+        req.userAuth = user;
+        next()
+    } else { 
+        const err = new Error("Token expired/invalid");
+        next(err);
+    }
+};
+
+module.exports = isLogin;
